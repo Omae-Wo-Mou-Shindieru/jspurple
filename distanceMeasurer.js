6d3af701111cc4ba462e002367ccbc58659be806
shamad <fuzzriff@gmail.com>
2023-07-19 01:23:32 +0300

    navigator task

---
+++ distanceMeasurer.js
@@ -0,0 +1,12 @@
+const addressLat = 10;
+const addressLong = 8;
+
+const positionLat = 2;
+const positionLong = 2;
+
+const legA = addressLat - positionLat;
+const legB = addressLong - positionLong;
+
+const hypotenuse = Math.sqrt(a ** 2 + b ** 2);
+
+console.log(distance);
