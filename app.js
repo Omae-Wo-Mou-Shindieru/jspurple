3c03a2c1b47261db73a4e4a09153bfa7a1151e39
shamad <fuzzriff@gmail.com>
2023-07-24 03:01:13 +0300

    code fixes, script moved to add.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("This is the way")
+const latProjection = 10;
+const longProjection = 8;
+
+const positionLatProjection = 2;
+const positionLongProjection = 2;
+
+const legA = latProjection - positionLatProjection;
+const legB = longProjection - positionLongProjection;
+
+const distance = Math.sqrt(legA ** 2 + legB ** 2);
+
+console.log(distance);
