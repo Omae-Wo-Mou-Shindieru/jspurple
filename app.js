48a348be7684b12da6bcc52428ff566d793fb9b1
shamad <fuzzriff@gmail.com>
2023-07-25 23:06:03 +0300

    done

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const password = "password";
-
 function shuffle(array) {
   const reversed = array.reverse();
   const temp = reversed[reversed.length - 2];
@@ -9,16 +7,18 @@ function shuffle(array) {
 }
 
 function crypto(string) {
-  if (string.length < 8) {
-    console.log("Пароль должен состоять из минимум 8 знаков");
-  }
   const arrFromStr = string.split("");
-  const firstHalf = arrFromStr.slice(0, arrFromStr.length / 2);
-  const secondHalf = arrFromStr.splice(arrFromStr.length / 2);
-  const strReversed = firstHalf.reverse();
-  const strShuffled = shuffle(secondHalf);
-  return strReversed.concat(strShuffled).join("");
+  const firstHalfReversed = arrFromStr
+    .slice(0, arrFromStr.length / 2)
+    .reverse();
+  const secondHalfShuffled = shuffle(arrFromStr.splice(arrFromStr.length / 2));
+  return firstHalfReversed.concat(secondHalfShuffled).join("");
 }
 
-function check(string, string) {
+function check(password, string) {
+  return password === crypto(string);
 }
+
+console.log(crypto("password"));
+console.log(check("ssapdorw", "password"));
+console.log(check("ssapdorw", "wrong"));
