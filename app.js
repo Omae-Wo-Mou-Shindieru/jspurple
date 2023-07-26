88ea292cd9b3e9165bd4bbc23a31826be3228db2
shamad <fuzzriff@gmail.com>
2023-07-26 18:21:43 +0300

    done

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 function shuffle(array) {
-  const reversed = array.reverse();
-  const temp = reversed[reversed.length - 2];
-  reversed[reversed.length - 2] = reversed[reversed.length - 3];
-  reversed[reversed.length - 3] = temp;
-  return reversed;
+    const reversed = array.reverse();
+    const temp = reversed[reversed.length - 2];
+    reversed[reversed.length - 2] = reversed[reversed.length - 3];
+    reversed[reversed.length - 3] = temp;
+    return reversed;
 }
 
 function crypto(string) {
-  const arrFromStr = string.split("");
-  const firstHalfReversed = arrFromStr
-    .slice(0, arrFromStr.length / 2)
-    .reverse();
-  const secondHalfShuffled = shuffle(arrFromStr.splice(arrFromStr.length / 2));
-  return firstHalfReversed.concat(secondHalfShuffled).join("");
+    const arrFromStr = string.split("");
+    const firstHalfReversed = arrFromStr.splice(0, arrFromStr.length / 2).reverse();
+    const secondHalfShuffled = shuffle(arrFromStr);
+    return firstHalfReversed.concat(secondHalfShuffled).join("");
 }
 
 function check(password, string) {
-  return password === crypto(string);
+    return password === crypto(string);
 }
 
 console.log(crypto("password"));
