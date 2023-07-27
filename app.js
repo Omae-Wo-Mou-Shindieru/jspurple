7fdbd39e10b05e8c540d62aa805ffd3dc9b0be48
shamad <fuzzriff@gmail.com>
2023-07-28 02:57:52 +0300

    review fixes

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-function shuffle(array) {
-    const reversed = array.reverse();
-    const temp = reversed[reversed.length - 2];
-    reversed[reversed.length - 2] = reversed[reversed.length - 3];
-    reversed[reversed.length - 3] = temp;
-    return reversed;
-}
-
 function crypto(string) {
-    const arrFromStr = string.split("");
-    const firstHalfReversed = arrFromStr.splice(0, arrFromStr.length / 2).reverse();
-    const secondHalfShuffled = shuffle(arrFromStr);
-    return firstHalfReversed.concat(secondHalfShuffled).join("");
+  const arrFromStr = string.split("");
+  if (arrFromStr.length < 4) {
+    return "Слишком короткий пароль";
+  }
+  const firstHalfReversed = arrFromStr
+    .splice(0, arrFromStr.length / 2)
+    .reverse();
+  const temp = arrFromStr[0];
+  arrFromStr[0] = arrFromStr[arrFromStr.length - 1];
+  arrFromStr[arrFromStr.length - 1] = temp;
+  return firstHalfReversed.concat(arrFromStr).join("");
 }
 
 function check(password, string) {
-    return password === crypto(string);
+  return password === crypto(string);
 }
 
 console.log(crypto("password"));
+console.log(crypto("pass"));
+console.log(crypto("pas"));
 console.log(check("ssapdorw", "password"));
 console.log(check("ssapdorw", "wrong"));
