e69c697676f1c3cbd1180a7db79abef1d67e85b0
shamad <fuzzriff@gmail.com>
2023-08-06 04:27:00 +0300

    formQueryParams function declared

---
+++ 13-query/index.js
@@ -0,0 +1,14 @@
+const object = {
+    search: "Вася",
+    take: 10
+}
+
+function formQueryParams(obj) {
+    const params = []
+    for ( const key in obj) {
+        params.push(key + "=" + obj[key])
+    }
+    return params.join("&")
+}
+
+console.log(formQueryParams(object));
