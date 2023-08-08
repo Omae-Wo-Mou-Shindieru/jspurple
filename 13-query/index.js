6648cbf5e234e5373338b4ab7fd550d5d4765101
shamad <fuzzriff@gmail.com>
2023-08-08 18:17:45 +0300

    object iteration changed: for in obj => for of Object.keys(obj)

--- 13-query/index.js
+++ 13-query/index.js
@@ -1,14 +1,14 @@
 const object = {
-    search: "Вася",
-    take: 10
-}
+  search: "Вася",
+  take: 10,
+};
 
 function formQueryParams(obj) {
-    const params = []
-    for ( const key in obj) {
-        params.push(key + "=" + obj[key])
-    }
-    return params.join("&")
+  const params = [];
+  for (const key of Object.keys(obj)) {
+    params.push(key + "=" + obj[key]);
+  }
+  return params.join("&");
 }
 
 console.log(formQueryParams(object));
