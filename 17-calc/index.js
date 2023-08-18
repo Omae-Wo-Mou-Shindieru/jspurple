1235dbd584998059018ad8ef34dc5a35cbdc3fb1
shamad <fuzzriff@gmail.com>
2023-08-18 11:24:51 +0300

    reviewed changes

--- 17-calc/index.js
+++ 17-calc/index.js
@@ -2,35 +2,40 @@
 
 const operand1 = document.querySelectorAll(".operand")[0];
 const operand2 = document.querySelectorAll(".operand")[1];
-const operations = document.querySelectorAll(".operator_btn");
 const resultSection = document.querySelector(".calc__result span");
+const operations = document.querySelector(".calc__buttons");
 
-function calculate(e) {
-  const operator = e.target.innerText;
-  const num1 = parseFloat(operand1.value);
-  const num2 = parseFloat(operand2.value);
-  if (isNaN(num1) || isNaN(num2)) {
-    resultSection.textContent = "Неверный ввод";
-    return;
-  }
+operations.addEventListener("click", (e) => {
+  if (e.target.className === "operator_btn") {
+    const operator = e.target.innerText;
+    const num1 = parseFloat(operand1.value);
+    const num2 = parseFloat(operand2.value);
 
-  let result;
-  switch (operator) {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*":
-      result = num1 * num2;
-      break;
-    case "/":
-      result = num1 / num2;
-      break;
-    default:
-      result = "Ошибка";
-      break;
+    if (isNaN(num1) || isNaN(num2)) {
+      resultSection.textContent = "Неверный ввод";
+      return;
+    }
+
+    let result;
+
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+      default:
+        result = "Ошибка";
+        break;
+    }
+
+    resultSection.innerText = result;
   }
-  resultSection.innerText = result;
-}
+});
